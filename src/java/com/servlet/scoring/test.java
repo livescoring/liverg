ac05b83626d32a8ee6acfb97b500b1d550668550
HazimDeveloper <hazimdeveloper@gmail.com>
2024-04-15 20:09:26 +0800

    first commit

---
+++ src/java/com/servlet/scoring/test.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.servlet.scoring;
+
+/**
+ *
+ * @author USER
+ */
+public class test {
+    
+}
